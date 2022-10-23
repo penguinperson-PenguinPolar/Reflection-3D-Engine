15f8229cdc39032bd7ab9009323e501678ae1347
Lars <penguinperson.pp@gmail.com>
2022-10-23 08:26:23 -0400

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,158 +1,145 @@
-import pygame,sys,re
-from math import *
+import pygame
+import numpy
 
-window = pygame.display.set_mode((800, 600))
-clock = pygame.time.Clock()
+class Engine:
+    def __init__(self, width, height, filename, maxfps=0):
+        self.width, self.height = width, height
+        self.fovV = numpy.pi/4
+        self.fovH = self.fovV*self.height/self.width
 
-projection_matrix = [[1,0,0],[0,1,0],[0,0,0]]
-
-#cube_points = [n for n in range(8)];cube_points[0] = [[-1], [-1], [1]];cube_points[1] = [[1],[-1],[1]];cube_points[2] = [[1],[1],[1]];cube_points[3] = [[-1],[1],[1]];cube_points[4] = [[-1],[-1],[-1]];cube_points[5] = [[1],[-1],[-1]];cube_points[6] = [[1],[1],[-1]];cube_points[7] = [[-1],[1],[-1]]
-
-scale = 100
-
-angle_x = angle_y = angle_z = 0
-angle_y = 60
-
-def connect_points(i, j, points):
-    pygame.draw.line(window, (255, 255, 255), (points[i][0], points[i][1]) , (points[j][0], points[j][1]))
-
-def multiply_m(a, b):
-    a_rows = len(a)
-    a_cols = len(a[0])
-    b_rows = len(b)
-    b_cols = len(b[0])
-    product = [[0 for _ in range(b_cols)] for _ in range(a_rows)]
-    if a_cols == b_rows:
-        for i in range(a_rows):
-            for j in range(b_cols):
-                for k in range(b_rows):
-                    product[i][j] += a[i][k] * b[k][j]
-    return product
-    
-class Button():
-	def __init__(self, x, y, image, scale):
-		width = image.get_width()
-		height = image.get_height()
-		self.image = pygame.transform.scale(image, (int(width * scale), int(height * scale)))
-		self.rect = self.image.get_rect()
-		self.rect.topleft = (x, y)
-		self.clicked = False
-
-	def draw(self, surface):
-		action = False
-		#get mouse position
-		pos = pygame.mouse.get_pos()
-
-		#check mouseover and clicked conditions
-		if self.rect.collidepoint(pos):
-			if pygame.mouse.get_pressed()[0] == 1 and self.clicked == False:
-				self.clicked = True
-				action = True
-
-		if pygame.mouse.get_pressed()[0] == 0:
-			self.clicked = False
-
-		#draw button on screen
-		surface.blit(self.image, (self.rect.x, self.rect.y))
-
-		return action
+        self.screen = pygame.display.set_mode((width, height))
+        self.running = True
+        self.clock = pygame.time.Clock()
+        self.maxfps = maxfps
+        self.surface = pygame.surface.Surface((self.width, self.height))
         
-class ImportOBJ:
-    def __init__(self, filename):
-        self.filename = filename
-    def importobj(self):
-        reComp = re.compile("(?<=^)(v |vn |vt |f )(.*)(?=$)", re.MULTILINE)
-        with open(self.filename) as f:
-            data = [txt.group() for txt in reComp.finditer(f.read())]
-        v_arr, vn_arr, f_arr = [], [], []
-        for line in data:
-            tokens = line.split(' ')
-            if tokens[0] == 'v':
-                v_arr.append([float(c) for c in tokens[1:]])
-            elif tokens[0] == 'vn':
-                vn_arr.append([float(c) for c in tokens[1:]])
-            elif tokens[0] == 'vt':
-                vn_arr.append([float(c) for c in tokens[1:]])
-            elif tokens[0] == 'f':
-                f_arr.append([[int(i) if len(i) else 0 for i in c.split('/')] for c in tokens[1:]])
+        self.points, self.triangles = self.read_obj(filename)
+
+        self.camera = numpy.asarray([13, 0.5, 2, 3.3, 0])
+        self.lightdirection = numpy.asarray([0, 1, 1])
+
+        self.z_order = numpy.zeros(len(self.triangles))
+        self.shade = self.z_order.copy()
+
+    def limitFPS(self):
+        self.clock.tick(self.maxfps)
+    
+    def getFPS(self):
+        print(self.clock.get_fps())
+
+    def movement(self):
+        if pygame.mouse.get_focused():
+            p_mouse = pygame.mouse.get_pos()
+            self.camera[3] = (self.camera[3] + 10*self.elapsed_time*numpy.clip((p_mouse[0]-self.width/2)/self.width, -0.2, .2))%(2*numpy.pi)
+            self.camera[4] = self.camera[4] + 10*self.elapsed_time*numpy.clip((p_mouse[1]-self.height/2)/self.height, -0.2, .2)
+            self.camera[4] = numpy.clip(self.camera[4], -.3, .3)
+    
+        pressed_keys = pygame.key.get_pressed()
+
+        if pressed_keys[ord('e')]: self.camera[1] += self.elapsed_time
+        elif pressed_keys[ord('q')]: self.camera[1] -= self.elapsed_time
+
+        if (pressed_keys[ord('w')] or pressed_keys[ord('s')]) and (pressed_keys[ord('a')] or pressed_keys[ord('d')]):
+            self.elapsed_time *= 0.707
+        
+        if pressed_keys[pygame.K_UP] or pressed_keys[ord('w')]:
+            self.camera[0] += self.elapsed_time*numpy.cos(self.camera[3])
+            self.camera[2] += self.elapsed_time*numpy.sin(self.camera[3])
+
+        elif pressed_keys[pygame.K_DOWN] or pressed_keys[ord('s')]:
+            self.camera[0] -= self.elapsed_time*numpy.cos(self.camera[3])
+            self.camera[2] -= self.elapsed_time*numpy.sin(self.camera[3])
+        
+        if pressed_keys[pygame.K_LEFT] or pressed_keys[ord('a')]:
+            self.camera[0] += self.elapsed_time*numpy.sin(self.camera[3])
+            self.camera[2] -= self.elapsed_time*numpy.cos(self.camera[3])
+        
+        elif pressed_keys[pygame.K_RIGHT] or pressed_keys[ord('d')]:
+            self.camera[0] -= self.elapsed_time*numpy.sin(self.camera[3])
+            self.camera[2] += self.elapsed_time*numpy.cos(self.camera[3])
+
+    def sort_triangles(self):
+        for i in range(len(self.triangles)):
+            triangle = self.triangles[i]
+
+            vet1 = self.points[triangle[1]][:3] - self.camera[:3]
+            vet2 = self.points[triangle[2]][:3] - self.camera[:3]
+
+            normal = numpy.cross(vet1, vet2)
+            normal = normal/numpy.sqrt(normal[0]*normal[0] + normal[1]*normal[1] + normal[2]*normal[2])
+
+            CameraRay = self.points[triangle[0]][:3] - self.camera[:3]
+            dist2cam = numpy.sqrt(CameraRay[0]*CameraRay[0] + CameraRay[1]*CameraRay[1] + CameraRay[2]*CameraRay[2])
+            CameraRay = CameraRay/dist2cam
+
+            if numpy.dot(normal, CameraRay) < 0:
+                self.z_order[i] = -dist2cam
+                self.shade[i] = 0.5*numpy.dot(self.lightdirection, normal)+0.5
+            else:
+                self.z_order[i] = 9999
+
+    def read_obj(self, fileName):
         vertices = []
-        for face in f_arr:
-            for tp in face:
-                for i in v_arr[tp[0]-1]:
-                    vertices.append([i])
-        return list(zip(*[iter(vertices)]*3))
+        triangles = []
+        f = open(fileName)
+        for line in f:
+            if line[:2] == "v ":
+                index1 = line.find(" ") + 1
+                index2 = line.find(" ", index1 + 1)
+                index3 = line.find(" ", index2 + 1)
+            
+                vertex = [float(line[index1:index2]), float(line[index2:index3]), float(line[index3:-1]), 1, 1]
+                vertices.append(vertex)
 
-cube_points = ImportOBJ("sphere.obj").importobj()
+            elif line[0] == "f":
+                index1 = line.find(" ") + 1
+                index2 = line.find(" ", index1 + 1)
+                index3 = line.find(" ", index2 + 1)
 
-fast_button = Button(120, 515, pygame.image.load("fast.png"), 4)
-slow_button = Button(550, 515, pygame.image.load("slow.png"), 4)
+                triangles.append([int(line[index1:index2]) - 1, int(line[index2:index3]) - 1, int(line[index3:-1]) - 1])
 
-rotate_speed = 0.5
+        f.close()
 
-debug = False
-weird = False
+        return numpy.asarray(vertices), numpy.asarray(triangles)
 
-pygame.font.init()
-font = pygame.font.SysFont('Comic Sans MS', 20)
+    def update(self):
+        self.surface.fill([50,127,200])
+        self.elapsed_time = self.clock.tick()*0.001
+        self.lightdirection = numpy.asarray([numpy.sin(pygame.time.get_ticks()/1000), 1, 1])
+        self.lightdirection = self.lightdirection/numpy.linalg.norm(self.lightdirection)
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                self.running = False
+            elif event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE:
+                self.running = False
+        self.project_points()
+        self.sort_triangles()
+        for index in numpy.argsort(self.z_order):
+            if self.z_order[index] == 9999: break
+            triangle = [self.points[self.triangles[index][0]][3:], self.points[self.triangles[index][1]][3:], self.points[self.triangles[index][2]][3:]]
+            color = self.shade[index]*numpy.abs(self.points[self.triangles[index][0]][:3])*45+25
+            pygame.draw.polygon(self.surface, color, triangle)
+        self.screen.blit(self.surface, (0, 0))
+        pygame.display.set_caption(str(round(1/(self.elapsed_time+1e-16), 1)) + ' ' + str(self.camera))
+        pygame.display.update()
+        self.movement()
 
-def drawText(text, x, y):
-    text_surface = font.render(text, False, (255, 255, 255))
-    window.blit(text_surface, (x,y))
+    def project_points(self):
+        for point in self.points:
+            h_angle_camera_point = numpy.arctan((point[2]-self.camera[2])/(point[0]-self.camera[0] + 1e-16))
+            if abs(self.camera[0]+numpy.cos(h_angle_camera_point)-point[0]) > abs(self.camera[0]-point[0]):
+                h_angle_camera_point = (h_angle_camera_point - numpy.pi) % (2*numpy.pi)
+            h_angle = (h_angle_camera_point - self.camera[3]) % (2*numpy.pi)
+            if h_angle > numpy.pi:
+                h_angle = h_angle - 2*numpy.pi
+            point[3] = self.width*h_angle/self.fovH+self.width/2
+            distance = numpy.sqrt((point[0]-self.camera[0])**2 + (point[1]-self.camera[1])**2 + (point[2]-self.camera[2])**2)
+            v_angle_camera_point = numpy.arcsin((self.camera[1]-point[1])/distance)
+            v_angle = (v_angle_camera_point - self.camera[4])%(2*numpy.pi)
+            if v_angle > numpy.pi:
+                v_angle = v_angle - 2*numpy.pi
+            point[4] = self.height*v_angle/self.fovV+self.height/2
 
-def truncate(n):
-    multiplier = 10 ** 3
-    return int(n * multiplier) / multiplier
-
-while True:
-    clock.tick(60)
-    
-    if weird == False:
-        window.fill((0,0,0))
-
-    if debug == True:
-        drawText(f"Mouse Pos: {pygame.mouse.get_pos()}", 5, 5)
-        drawText(f"Rotation speed: {int(rotate_speed*100)}", 5, 35)
-        drawText(f"XYZ Rotation: {truncate(angle_x)}, {truncate(angle_y)}, {truncate(angle_z)}", 5, 65)
-
-    if fast_button.draw(window):
-        rotate_speed += 0.5
-    if slow_button.draw(window):
-        rotate_speed -= 0.5
-
-    angle_x += rotate_speed
-    angle_y += rotate_speed
-    angle_z += rotate_speed
-
-    rotation_x = [[1, 0, 0],[0, cos(angle_x/60), -sin(angle_x/60)],[0, sin(angle_x/60), cos(angle_x/60)]]
-
-    rotation_y = [[cos(angle_y/60), 0, sin(angle_y/60)],[0, 1, 0],[-sin(angle_y/60), 0, cos(angle_y/60)]]
-
-    rotation_z = [[cos(angle_z/60), -sin(angle_z/60), 0],[sin(angle_z/60), cos(angle_z/60), 0],[0, 0, 1]]
-
-    points = [0 for _ in range(len(cube_points))]
-
-    i=0
-    for point in cube_points:
-        rotate_x = multiply_m(rotation_x, point)
-        rotate_y = multiply_m(rotation_y, rotate_x)
-        rotate_z = multiply_m(rotation_z, rotate_y)
-        point_2d = multiply_m(projection_matrix, rotate_z)
-
-        x = (point_2d[0][0] * scale) + 400
-        y = (point_2d[1][0] * scale) + 300
-        
-        points[i] = (x,y)
-        i+=1
-
-        pygame.draw.circle(window, (255, 0, 0), (x,y), 5)
-
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            pygame.quit()
-            sys.exit()
-        if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_d:
-                debug = not debug
-            if event.key == pygame.K_j:
-                weird = not weird
-    pygame.display.update()
+engine = Engine(800, 600, input("OBJ model: "))
+while engine.running:
+    engine.update()
