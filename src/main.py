1cfb7a13ed6704f22ac48b1fae65303f5cae7f43
Lars <penguinperson.pp@gmail.com>
2022-11-05 16:43:48 -0400

    2.1

--- src/main.py
+++ src/main.py
@@ -1,62 +1,87 @@
-import pygame
-import numpy
+# Reflection 3D Engine
+# Also it doesn't support reflection lol
+
+# Import packages
+
+import pygame, numpy, threading, numba
+
+# Dot multiplication
+
+@numba.njit()
+def dot_3d(arr1, arr2):
+    return arr1[0]*arr2[0] + arr1[1]*arr2[1] + arr1[2]*arr2[2]
+
+# Engine Class
 
 class Engine:
+    # __slots__ slightly increases performance
+    __slots__ = ("width", "height", "filename", "maxfps", "fovV", "fovH", "screen", "running", "surface", "points", "triangles", "camera", "lightdirection", "z_order", "shade", "clock", "elapsed_time")
+    
+    # Define variables
+    
     def __init__(self, width, height, filename, maxfps=0):
-        self.width, self.height = width, height
-        self.fovV = numpy.pi/4
+        self.width, self.height = width, height # Window height
+        self.fovV = numpy.pi/4 # 45 Degrees
         self.fovH = self.fovV*self.height/self.width
 
-        self.screen = pygame.display.set_mode((width, height))
-        self.running = True
-        self.clock = pygame.time.Clock()
-        self.maxfps = maxfps
-        self.surface = pygame.surface.Surface((self.width, self.height))
+        self.screen = pygame.display.set_mode((width, height)) # Pygame screen
+        self.running = True # True if running
+        self.clock = pygame.time.Clock() # Clock to limit FPS and get elapsed time
+        self.maxfps = maxfps # Maximum FPS
+        self.surface = pygame.surface.Surface((self.width, self.height)) # Surface to draw to screen
         
-        self.points, self.triangles = self.read_obj(filename)
+        self.points, self.triangles = self.read_obj(filename) # Export from OBJ file
 
-        self.camera = numpy.asarray([13, 0.5, 2, 3.3, 0])
-        self.lightdirection = numpy.asarray([0, 1, 1])
+        self.camera = numpy.asarray([13, 0.5, 2, 3.3, 0]) # Camera Pos and Rotation
+        self.lightdirection = numpy.asarray([0, 1, 1]) # Light Direction
 
-        self.z_order = numpy.zeros(len(self.triangles))
-        self.shade = self.z_order.copy()
+        self.z_order = numpy.zeros(len(self.triangles)) # Order of triangles
+        self.shade = self.z_order.copy() # Color triangles
+
+        self.texture = pygame.surfarray.array3d(pygame.image.load("texture.png")) # Texture
 
     def limitFPS(self):
-        self.clock.tick(self.maxfps)
+        self.clock.tick(self.maxfps) # Limit FPS
     
     def getFPS(self):
-        print(self.clock.get_fps())
+        print(self.clock.get_fps()) # Print FPS to the terminal
 
     def movement(self):
-        if pygame.mouse.get_focused():
-            p_mouse = pygame.mouse.get_pos()
-            self.camera[3] = (self.camera[3] + 10*self.elapsed_time*numpy.clip((p_mouse[0]-self.width/2)/self.width, -0.2, .2))%(2*numpy.pi)
-            self.camera[4] = self.camera[4] + 10*self.elapsed_time*numpy.clip((p_mouse[1]-self.height/2)/self.height, -0.2, .2)
-            self.camera[4] = numpy.clip(self.camera[4], -.3, .3)
-    
-        pressed_keys = pygame.key.get_pressed()
+        pressed_keys = pygame.key.get_pressed() # A list of pressed keys
 
-        if pressed_keys[ord('e')]: self.camera[1] += self.elapsed_time
-        elif pressed_keys[ord('q')]: self.camera[1] -= self.elapsed_time
+        if pressed_keys[ord('e')]: self.camera[1] += self.elapsed_time # Up
+        elif pressed_keys[ord('q')]: self.camera[1] -= self.elapsed_time # Down
 
         if (pressed_keys[ord('w')] or pressed_keys[ord('s')]) and (pressed_keys[ord('a')] or pressed_keys[ord('d')]):
-            self.elapsed_time *= 0.707
+            self.elapsed_time *= 0.707 # Move time when the camera moves
         
-        if pressed_keys[pygame.K_UP] or pressed_keys[ord('w')]:
+        if pressed_keys[ord('w')]: # Forward
             self.camera[0] += self.elapsed_time*numpy.cos(self.camera[3])
             self.camera[2] += self.elapsed_time*numpy.sin(self.camera[3])
 
-        elif pressed_keys[pygame.K_DOWN] or pressed_keys[ord('s')]:
+        elif pressed_keys[ord('s')]: # Backward
             self.camera[0] -= self.elapsed_time*numpy.cos(self.camera[3])
             self.camera[2] -= self.elapsed_time*numpy.sin(self.camera[3])
         
-        if pressed_keys[pygame.K_LEFT] or pressed_keys[ord('a')]:
+        if pressed_keys[ord('a')]: # Left
             self.camera[0] += self.elapsed_time*numpy.sin(self.camera[3])
             self.camera[2] -= self.elapsed_time*numpy.cos(self.camera[3])
         
-        elif pressed_keys[pygame.K_RIGHT] or pressed_keys[ord('d')]:
+        elif pressed_keys[ord('d')]: # Right
             self.camera[0] -= self.elapsed_time*numpy.sin(self.camera[3])
             self.camera[2] += self.elapsed_time*numpy.cos(self.camera[3])
+        
+        if pressed_keys[pygame.K_UP]: # Rotate Up
+            self.camera[4] -= self.elapsed_time*0.5
+
+        elif pressed_keys[pygame.K_DOWN]: # Rotate Down
+            self.camera[4] += self.elapsed_time*0.5
+        
+        if pressed_keys[pygame.K_LEFT]: # Rotate Left
+            self.camera[3] -= self.elapsed_time*0.5
+        
+        elif pressed_keys[pygame.K_RIGHT]: # Rotate Right
+            self.camera[3] += self.elapsed_time*0.5
 
     def sort_triangles(self):
         for i in range(len(self.triangles)):
@@ -72,7 +97,7 @@ class Engine:
             dist2cam = numpy.sqrt(CameraRay[0]*CameraRay[0] + CameraRay[1]*CameraRay[1] + CameraRay[2]*CameraRay[2])
             CameraRay = CameraRay/dist2cam
 
-            if numpy.dot(normal, CameraRay) < 0:
+            if dot_3d(normal, CameraRay) < 0:
                 self.z_order[i] = -dist2cam
                 self.shade[i] = 0.5*numpy.dot(self.lightdirection, normal)+0.5
             else:
@@ -112,17 +137,17 @@ class Engine:
                 self.running = False
             elif event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE:
                 self.running = False
-        self.project_points()
+        threading.Thread(target=self.project_points).start()
         self.sort_triangles()
         for index in numpy.argsort(self.z_order):
             if self.z_order[index] == 9999: break
             triangle = [self.points[self.triangles[index][0]][3:], self.points[self.triangles[index][1]][3:], self.points[self.triangles[index][2]][3:]]
             color = self.shade[index]*numpy.abs(self.points[self.triangles[index][0]][:3])*45+25
             pygame.draw.polygon(self.surface, color, triangle)
-        self.screen.blit(self.surface, (0, 0))
-        pygame.display.set_caption(str(round(1/(self.elapsed_time+1e-16), 1)) + ' ' + str(self.camera))
+        threading.Thread(target=self.screen.blit, args=(self.surface, (0, 0))).start()
+        pygame.display.set_caption(f"{int(self.clock.get_fps())} FPS")
         pygame.display.update()
-        self.movement()
+        threading.Thread(target=self.movement).start()
 
     def project_points(self):
         for point in self.points:
@@ -140,6 +165,6 @@ class Engine:
                 v_angle = v_angle - 2*numpy.pi
             point[4] = self.height*v_angle/self.fovV+self.height/2
 
-engine = Engine(800, 600, input("OBJ model: "))
+engine = Engine(800, 600, "teapot.obj")
 while engine.running:
     engine.update()
